b13a932fcfc0464d05efeaab314d7cf240dc0f9a
Tyler Auerbeck <tauerbec@redhat.com>
2021-04-14 10:15:29 -0400

    Add tests

--- _test/conftest.sh
+++ _test/conftest.sh
@@ -184,3 +184,14 @@ setup_file() {
   print_info "${status}" "${output}" "${cmd}" "${tmp}"
   [ "$status" -eq 0 ]
 }
+
+@test "charts/kopf" {
+  tmp=$(helm_template "charts/tekton-demo")
+
+  namespaces=$(get_rego_namespaces "ocp\.deprecated\.*")
+  cmd="conftest test ${tmp} --output tap ${namespaces}"
+  run ${cmd}
+
+  print_info "${status}" "${output}" "${cmd}" "${tmp}"
+  [ "$status" -eq 0 ]
+}
