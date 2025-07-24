43a68e2917b1b7469117c6b96fab3ed753490eb3
Randy E. Oyarzabal <randyo@redhat.com>
2025-07-23 23:25:32 -0500

    Added feature to trust hosts, to ignore SSL errors.

--- charts/operators-installer/files/pip-reqs.sh
+++ charts/operators-installer/files/pip-reqs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Use environment variables instead of positional parameters
+# Use environment variables
 PYTHON_INDEX_URL=${PYTHON_INDEX_URL:-https://pypi.org/simple/}
 PYTHON_EXTRA_INDEX_URL=${PYTHON_EXTRA_INDEX_URL:-https://pypi.org/simple/}
+IGNORE_SSL_ERRORS=${IGNORE_SSL_ERRORS:-false}
 
 # Extract hostnames
 INDEX_HOST=$(echo $PYTHON_INDEX_URL | sed 's|https\?://||' | cut -d'/' -f1)
@@ -11,8 +12,16 @@ EXTRA_INDEX_HOST=$(echo $PYTHON_EXTRA_INDEX_URL | sed 's|https\?://||' | cut -d'
 
 echo "PYTHON_INDEX_URL: $PYTHON_INDEX_URL"
 echo "PYTHON_EXTRA_INDEX_URL: $PYTHON_EXTRA_INDEX_URL"
-echo "INDEX_HOST: $INDEX_HOST"
-echo "EXTRA_INDEX_HOST: $EXTRA_INDEX_HOST"
+echo "TRUST_HOSTS: $TRUST_HOSTS"
+
+# Build trusted-host arguments conditionally
+if [ "$TRUST_HOSTS" = "true" ]; then
+    TRUSTED_HOST_ARGS="--trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host $INDEX_HOST --trusted-host $EXTRA_INDEX_HOST"
+    echo "SSL verification disabled for pip"
+else
+    TRUSTED_HOST_ARGS=""
+    echo "SSL verification enabled for pip"
+fi
 
 echo "Creating virtual environment..."
 python3 -m venv /tmp/venv
@@ -21,20 +30,11 @@ echo "Activating virtual environment and installing packages..."
 source /tmp/venv/bin/activate
 
 echo "Upgrading pip..."
-python3 -m pip install --upgrade pip \
-    --trusted-host pypi.org \
-    --trusted-host pypi.python.org \
-    --trusted-host files.pythonhosted.org \
-    --trusted-host $INDEX_HOST \
-    --trusted-host $EXTRA_INDEX_HOST
+python3 -m pip install --upgrade pip $TRUSTED_HOST_ARGS
 
 echo "Installing packages..."
 python3 -m pip install openshift-client semver==2.13.0 \
-    --trusted-host pypi.org \
-    --trusted-host pypi.python.org \
-    --trusted-host files.pythonhosted.org \
-    --trusted-host $INDEX_HOST \
-    --trusted-host $EXTRA_INDEX_HOST \
+    $TRUSTED_HOST_ARGS \
     --index-url $PYTHON_INDEX_URL \
     --extra-index-url $PYTHON_EXTRA_INDEX_URL
 
